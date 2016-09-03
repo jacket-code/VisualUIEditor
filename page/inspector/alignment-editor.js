904b752ae1597a0991dfe3310f49b0a4d06d8ca1
tickbh <tickdream125@hotmail.com>
2016-09-03 15:23:13 +0800

    widget editor

--- page/inspector/alignment-editor.js
+++ page/inspector/alignment-editor.js
@@ -10,43 +10,29 @@ Editor.polymerElement({
     _is: function(t) {
         return !!t
     },
-    _compose: function(t, e) {
-        t = t || 0, e || (t *= 100);
-        var i = e ? "px" : "%";
-        return "" + t.toFixed(2) + i
-    },
-    _decompose: function(t) {
-        var e;
-        return t.endsWith("%") || t.endsWith("％") ? (t = t.slice(0, -1), e = !1) : (t.endsWith("px") && (t = t.slice(0, -2)), e = !0), t = "" === t ? 0 : parseFloat(t), e || (t /= 100), {
-            value: t,
-            isAbsolute: e
+    _onTopChecked: function(t, e) {
+        if(e.value == true) {
+            this.set("value.isAlignVerticalCenter", false)
+            this.$.top_input.confirm();
         }
     },
-    _changeMargin: function(t, e, i) {
-        if (this.target) {
-            var n = this._decompose(t);
-            if (isNaN(n.value)) return Editor.warn('Invalid input: "%s"', t), !1;
-            n.value !== this.target[e].value && this.set("value." + e + ".value", n.value), n.isAbsolute !== this.target[i].value && this.set("value." + i + ".value", n.isAbsolute)
+    _onLeftChecked: function(t, e) {
+        if(e.value == true) {
+            this.set("value.isAlignHorizontalCenter", false)
+            this.$.left_input.confirm();
         }
-        return !0
     },
-    _onTopChanged: function(t, e) {
-        this._changeMargin(e.value, "top", "isAbsoluteTop")
+    _onRightChecked: function(t, e) {
+        if(e.value == true) {
+            this.set("value.isAlignHorizontalCenter", false)
+            this.$.right_input.confirm();
+        }
     },
-    _onLeftChanged: function(t, e) {
-        this._changeMargin(e.value, "left", "isAbsoluteLeft")
-    },
-    _onRightChanged: function(t, e) {
-        this._changeMargin(e.value, "right", "isAbsoluteRight")
-    },
-    _onBottomChanged: function(t, e) {
-        this._changeMargin(e.value, "bottom", "isAbsoluteBottom")
-    },
-    _onLeftRightChecked: function(t, e) {
-        this.set("value.isAlignHorizontalCenter", false)
-    },
-    _onTopBottomChecked: function(t, e) {
-        this.set("value.isAlignVerticalCenter", false)
+    _onBottomChecked: function(t, e) {
+        if(e.value == true) {
+            this.set("value.isAlignVerticalCenter", false)
+            this.$.bottom_input.confirm();
+        }
     },
     _onHorizontalCenterChecked: function(t, e) {
         if(e.value) {
