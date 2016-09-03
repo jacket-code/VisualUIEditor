e855b4fbf3fe289cb926780d0ac464906dff22bd
tickbh <tickdream125@hotmail.com>
2016-09-03 17:19:59 +0800

    add editor

--- page/inspector/alignment-editor.js
+++ page/inspector/alignment-editor.js
@@ -7,34 +7,54 @@ Editor.polymerElement({
             notify: true
         }
     },
+
+    ready: function () {
+        this._isReady = true;
+    },
+
     _is: function(t) {
         return !!t
     },
     _onTopChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value == true) {
             this.set("value.isAlignVerticalCenter", false)
             this.$.top_input.confirm();
         }
     },
     _onLeftChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value == true) {
             this.set("value.isAlignHorizontalCenter", false)
             this.$.left_input.confirm();
         }
     },
     _onRightChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value == true) {
             this.set("value.isAlignHorizontalCenter", false)
             this.$.right_input.confirm();
         }
     },
     _onBottomChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value == true) {
             this.set("value.isAlignVerticalCenter", false)
             this.$.bottom_input.confirm();
         }
     },
     _onHorizontalCenterChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value) {
             this.set("value.isAlignLeft", false);
             this.set("value.isAlignRight", false);
@@ -42,6 +62,9 @@ Editor.polymerElement({
         }
     },
     _onVerticalCenterChecked: function(t, e) {
+        if(!this._isReady) {
+            return;
+        }
         if(e.value) {
             this.set("value.isAlignTop", false);
             this.set("value.isAlignBottom", false);
