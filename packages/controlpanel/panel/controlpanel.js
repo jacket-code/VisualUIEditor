6a78e296dbb64c7b494b5dc4fba39071743763b2
tickbh <tickdream125@hotmail.com>
2016-09-05 20:04:42 +0800

    fix node info

--- packages/controlpanel/panel/controlpanel.js
+++ packages/controlpanel/panel/controlpanel.js
@@ -19,24 +19,9 @@
 
     },
 
-    _getItems: function() {
-        return this.show_items;
-    },
 
     ready: function () {
-        let show_items = [
-            {icon:"res/control/Node.png", name:"Node", tag: 0},
-            {icon:"res/control/Sprite.png", name:"Sprite", tag: 1},
-            {icon:"res/control/Label.png", name:"LabelTTF", tag: 2},
-            {icon:"res/control/Label.png", name:"LabelAtlas", tag: 3},
-            {icon:"res/control/Scale9.png", name:"Scale9", tag: 4},
-            {icon:"res/control/Input.png", name:"Input", tag: 5},
-            {icon:"res/control/SliderBar.png", name:"Slider", tag: 6},
-            {icon:"res/control/Button.png", name:"Button", tag: 7},
-            {icon:"res/control/CheckBox.png", name:"CheckBox", tag: 8},
-            {icon:"res/control/CheckBox.png", name:"ListView", tag: 9},
-        ];
-
+        let show_items = [];
         var nodes=GetExtllNodeControls();
         for (name in nodes)
         {
