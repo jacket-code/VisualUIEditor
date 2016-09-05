9a3b1204b16d56c8ca42cab995e563a17d5fa97b
tickbh <tickdream125@hotmail.com>
2016-09-05 19:55:24 +0800

    ext button and ext checkBox

---
+++ ext/ExtButton.js
@@ -0,0 +1,191 @@
+
+let ExtButton = {};
+
+ExtButton.name = "UIButton";
+ExtButton.icon = "res/control/Button.png";
+ExtButton.tag = 7;
+ExtButton.defNormal = "res/default/ButtonNormal.png";
+
+ExtButton.GenEmptyNode = function() {
+    node = new ccui.Button(ExtButton.defNormal);
+    node._bgNormal = ExtButton.defNormal;
+    node._className = ExtButton.name;
+    return node;
+};
+
+ExtButton.GenNodeByData = function(data, parent) {
+    return this.GenEmptyNode();
+};
+
+ExtButton.SetNodePropByData = function(node, data, parent) {
+    (data["scale9Enable"]) && (node.setScale9Enabled(data["scale9Enable"]));
+    setNodeSpriteFrame("bgNormal", data["bgNormal"], node, node.loadTextureNormal);
+    setNodeSpriteFrame("bgSelect", data["bgSelect"], node, node.loadTexturePressed);
+    setNodeSpriteFrame("bgDisable", data["bgDisable"], node, node.loadTextureDisabled);
+    
+    (data["titleText"]) && (node.setTitleText(data["titleText"]));
+    (data["fontName"]) && (node.setTitleFontName(data["fontName"]));
+    (data["fontSize"]) && (node.setTitleFontSize(data["fontSize"]));
+    (data["fontColor"]) && (node.setTitleColor(covertToColor(data["fontColor"])));
+};
+
+ExtButton.ExportNodeData = function(node, data) {
+    (node.isScale9Enabled()) && (data["scale9Enable"] = node.isScale9Enabled());
+    (node._bgNormal) && (data["bgNormal"] = node._bgNormal);
+    (node._bgSelect) && (data["bgSelect"] = node._bgSelect);
+    (node._bgDisable) && (data["bgDisable"] = node._bgDisable);
+    (node.getTitleText().length > 0) && (data["titleText"] = node.getTitleText());
+    (node.getTitleFontName().length > 0) && (data["fontName"] = node.getTitleFontName());
+    (node.getTitleFontSize() > 0) && (data["fontSize"] = node.getTitleFontSize());
+    if(!cc.colorEqual(node.getTitleColor(), cc.color.WHITE)) {
+        let color = node.getTitleColor();
+        data["fontColor"] = [color.r, color.g, color.b, color.a];
+    }
+};
+
+ExtButton.SetPropChange = function(control, path, value) {
+    if(path == "isScale9Enabled") {
+        control._node.setScale9Enabled(value);
+    } else if(path == "bgNormal") {
+        SetSpriteFrame(control._node, path, value, "res/default/ButtonNormal.png", control._node.loadTextureNormal);
+    } else if(path == "bgSelect") {
+        SetSpriteFrame(control._node, path, value, "res/default/ButtonSelect.png", control._node.loadTexturePressed);
+    } else if(path == "bgDisable") {
+        SetSpriteFrame(control._node, path, value, "res/default/ButtonDisable.png", control._node.loadTextureDisabled);
+    } else if(path == "titleText") {
+        control._node.setTitleText(value);
+    } else if(path == "fontName") {
+        control._node.setTitleFontName(value);
+    } else if(path == "fontSize") {
+        control._node.setTitleFontSize(value);
+    } else if(path == "fontColor") {
+        control._node.setTitleColor(new cc.Color(value.r, value.g, value.b, value.a));
+    } else {
+        control._node[path] = value;
+    }
+};
+
+ExtButton.ExportData = function(node) {
+    this._node = node;
+}
+
+ExtButton.ExportData.prototype = {
+    __displayName__: "Button",
+    __type__: "cc.Button",
+
+    get isScale9Enabled() {
+        return {
+            path: "isScale9Enabled",
+            type: "check",
+            name: "isScale9Enabled",
+            attrs: {
+            },
+            value: this._node.isScale9Enabled(),
+        };
+    },
+
+    get bgNormal() {
+        return {
+            path: "bgNormal",
+            type: "fire-asset",
+            name: "bgNormal",
+            attrs: {
+            },
+            value: this._node._bgNormal,
+        };
+    },
+
+    get bgSelect() {
+        return {
+            path: "bgSelect",
+            type: "fire-asset",
+            name: "bgSelect",
+            attrs: {
+            },
+            value: this._node._bgSelect,
+        };
+    },
+
+    get bgDisable() {
+        return {
+            path: "bgDisable",
+            type: "fire-asset",
+            name: "bgDisable",
+            attrs: {
+            },
+            value: this._node._bgDisable,
+        };
+    },
+
+    get titleText() {
+        return {
+            path: "titleText",
+            type: "string",
+            name: "titleText",
+            attrs: {
+            },
+            value: this._node.getTitleText(),
+        };
+    },
+
+    get fontName() {
+        return {
+            path: "fontName",
+            type: "string",
+            name: "fontName",
+            attrs: {
+            },
+            value: this._node.getTitleFontName(),
+        };
+    },
+
+    get fontSize() {
+        return {
+            path: "fontSize",
+            type: "number",
+            name: "fontSize",
+            attrs: {
+                expand: true,
+                step: 1,
+                precision: 0,
+                min: 0,
+                max: 72,
+            },
+            value: this._node.getTitleFontSize(),
+        };
+    },
+
+    get fontColor() {
+        return {
+            path: "fontColor",
+            type: "color",
+            name: "fontColor",
+            attrs: {
+            },
+            value: this._node.getTitleColor(),
+        };
+    },
+
+    get __props__() {
+        return [
+            this.isScale9Enabled,
+            this.bgNormal,
+            this.bgSelect,
+            this.bgDisable,
+            this.titleText,
+            this.fontName,
+            this.fontSize,
+            this.fontColor,
+        ];
+    }
+}
+
+ExtButton.PropComps = function(node) {
+    let datas = [ new WidgetData(node) ];
+    datas.push(new ExtButton.ExportData(node));
+    return datas;
+};
+
+module.exports = ExtButton;
+
+RegisterExtNodeControl(ExtButton.name, ExtButton);
---
+++ ext/ExtCheckBox.js
@@ -0,0 +1,162 @@
+
+let ExtCheckBox = {};
+
+ExtCheckBox.name = "UICheckBox";
+ExtCheckBox.icon = "res/control/CheckBox.png";
+ExtCheckBox.tag = 8;
+ExtCheckBox.defNormal = "res/default/ButtonNormal.png";
+
+ExtCheckBox.GenEmptyNode = function() {
+    let back = "res/default/CheckBoxNormal.png";
+    let backSelect = "res/default/CheckBoxSelect.png";
+    let active = "res/default/CheckBoxNodeNormal.png";
+    let backDisable = "res/default/CheckBoxDisable.png";
+    let activeDisable = "res/default/CheckBoxNodeDisable.png";
+    node = new ccui.CheckBox(back, backSelect, active, backDisable, activeDisable);
+    node._back = back;
+    node._backSelect = backSelect;
+    node._active = active;
+    node._backDisable = backDisable;
+    node._activeDisable = activeDisable;
+    node.setSelected(true);
+    node._className = ExtCheckBox.name;
+    return node;
+};
+
+ExtCheckBox.GenNodeByData = function(data, parent) {
+    return this.GenEmptyNode();
+};
+
+ExtCheckBox.SetNodePropByData = function(node, data, parent) {
+    setNodeSpriteFrame("back", data["back"], node, node.loadTextureBackGround);
+    setNodeSpriteFrame("backSelect", data["backSelect"], node, node.loadTextureBackGroundSelected);
+    setNodeSpriteFrame("active", data["active"], node, node.loadTextureFrontCross);
+    setNodeSpriteFrame("backDisable", data["backDisable"], node, node.loadTextureBackGroundDisabled);
+    setNodeSpriteFrame("activeDisable", data["activeDisable"], node, node.loadTextureFrontCrossDisabled);
+
+    (data["select"]) && (node.setSelected(data["select"]));
+};
+
+ExtCheckBox.ExportNodeData = function(node, data) {
+    (node._back) && (data["back"] = node._back);
+    (node._backSelect) && (data["backSelect"] = node._backSelect);
+    (node._active) && (data["active"] = node._active);
+    (node._backDisable) && (data["backDisable"] = node._backDisable);
+    (node._activeDisable) && (data["activeDisable"] = node._activeDisable);
+    data["select"] = node.isSelected();
+};
+
+ExtCheckBox.SetPropChange = function(control, path, value) {
+    if(path == "back") {
+        SetSpriteFrame(control._node, path, value, "res/default/CheckBoxNormal.png", control._node.loadTextureBackGround);
+    } else if(path == "backSelect") {
+        SetSpriteFrame(control._node, path, value, "res/default/CheckBoxSelect.png", control._node.loadTextureBackGroundSelected);
+    } else if(path == "active") {
+        SetSpriteFrame(control._node, path, value, "res/default/CheckBoxNodeNormal.png", control._node.loadTextureFrontCross);
+    } else if(path == "backDisable") {
+        SetSpriteFrame(control._node, path, value, "res/default/CheckBoxDisable.png", control._node.loadTextureBackGroundDisabled);
+    } else if(path == "activeDisable") {
+        SetSpriteFrame(control._node, path, value, "res/default/CheckBoxNodeDisable.png", control._node.loadTextureFrontCrossDisabled);
+    } else if(path == "select") {
+        control._node.setSelected(value);
+    }
+};
+
+ExtCheckBox.ExportData = function(node) {
+    this._node = node;
+}
+
+ExtCheckBox.ExportData.prototype = {
+__editor__ : {
+        "inspector1": "cc.CheckBox",
+    },
+    __displayName__: "CheckBox",
+    __type__: "cc.CheckBox",
+
+    get back() {
+        return {
+            path: "back",
+            type: "fire-asset",
+            name: "back",
+            attrs: {
+            },
+            value: this._node._back,
+        };
+    },
+
+    get backSelect() {
+        return {
+            path: "backSelect",
+            type: "fire-asset",
+            name: "backSelect",
+            attrs: {
+            },
+            value: this._node._backSelect,
+        };
+    },
+
+    get active() {
+        return {
+            path: "active",
+            type: "fire-asset",
+            name: "active",
+            attrs: {
+            },
+            value: this._node._active,
+        };
+    },
+
+    get backDisable() {
+        return {
+            path: "backDisable",
+            type: "fire-asset",
+            name: "backDisable",
+            attrs: {
+            },
+            value: this._node._backDisable,
+        };
+    },
+
+    get activeDisable() {
+        return {
+            path: "activeDisable",
+            type: "fire-asset",
+            name: "activeDisable",
+            attrs: {
+            },
+            value: this._node._activeDisable,
+        };
+    },
+
+    get select() {
+        return {
+            path: "select",
+            type: "check",
+            name: "select",
+            attrs: {
+            },
+            value: this._node.isSelected(),
+        };
+    },
+
+    get __props__() {
+        return [
+            this.back,
+            this.backSelect,
+            this.active,
+            this.backDisable,
+            this.activeDisable,
+            this.select,
+        ];
+    }
+}
+
+ExtCheckBox.PropComps = function(node) {
+    let datas = [ new WidgetData(node) ];
+    datas.push(new ExtCheckBox.ExportData(node));
+    return datas;
+};
+
+module.exports = ExtCheckBox;
+
+RegisterExtNodeControl(ExtCheckBox.name, ExtCheckBox);
